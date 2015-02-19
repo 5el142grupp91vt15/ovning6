756e5f89eca1ec895826efa131855b68eb9762f6
Tobias Lindberg <tobbbe123@gmail.com>
2015-02-19 13:02:01 +0100

    libresistance
    libresistance.c & libresistance.h

--- libresistance.c
+++ libresistance.c
@@ -1,7 +1,31 @@
+#include <stdio.h>
 #include "libresistance.h"
 
-float calc_resistance(int count, char conn, float *array)
-{
-	return -1;
-}
 
+float calc_resistance(int count, char conn, float *array){
+    float result = 0.0;
+    
+    if(conn != 'P' && conn != 'S') {
+        return -1;
+    }
+    
+    if(array == 0) {
+        return -1;
+    }
+    
+    for(int i = 0; i < count; i++) {
+        if(conn == 'P') {
+            if(array[i] == 0){
+                return 0;
+            }
+            result += (1/array[i]);
+            if(i == count-1){
+                result = 1/result;
+            }  
+        } 
+        else{
+            result += array[i];
+        }
+    }
+    return result;  
+}
