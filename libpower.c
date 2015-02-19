87bee0dabd03459bd48c95fbe70213b462b2fdea
Your Name <you@example.com>
2015-02-19 12:55:55 +0100

    Power
    libpower.c och libpower.h

--- libpower.c
+++ libpower.c
@@ -1,13 +1,28 @@
 #include "libpower.h"
 #include "libresistance.h"
 
-float calc_power_r(float volt, float resistance)
-{
-	return -1;
+float calc_power_r(float volt, float resistance) {
+    
+    float result = 0;
+    
+    if(volt == 0 || resistance == 0) {
+        return 0;
+    }
+    
+    result = (volt * volt) / resistance;
+    
+    return result;
 }
 
-float calc_power_i(float volt, float current) 
-{
-	return -1;
+float calc_power_i(float volt, float current) {
+    
+    if(volt == 0 || current == 0) {
+        return 0;
+    }
+    
+    float result = 0;
+    
+    result = volt * current;
+    
+    return result;
 }
-
