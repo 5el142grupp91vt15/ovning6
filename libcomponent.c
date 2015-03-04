d47ae459d70f9933536fb2bd3dbd3fd61ec06332
axon <axon@ubuntu.axon>
2015-03-04 22:27:02 +0100

    Optimering och bugfix i libcomponent

--- libcomponent.c
+++ libcomponent.c
@@ -1,40 +1,49 @@
 #include "libcomponent.h"
+#include <float.h>
+#include <math.h>
 
 int e_resistance(float orig_resistance, float *res_array)
 {
 	int N = 13;
 	float E12[13] = {82.0,68.0,56.0,47.0,39.0,33.0,27.0,22.0,18.0,15.0,12.0,10.0,0.0};
-	float R[3] =  {0.0};
-	float R_t[3] = {0.0};
-	float err = 82;
-	float err_t = 82;
+	int idx[3] = {0};
+	float err = FLT_MAX;
+	float err_t = FLT_MAX;
+	int exit_search = 0;
 	
 	int i,j,k = 0;
 	for(i = 0; i < N; i++)  {
-		R_t[0] = E12[i];
 		for(j = 0; j < N; j++)  {
-			R_t[1] = E12[j];
 			for(k = 0; k < N; k++)  {
-				R_t[2] = E12[k];
-				err_t = abs(orig_resistance-R_t[0]-R_t[1]-R_t[2]);
+				err_t = fabs(orig_resistance-E12[i]-E12[j]-E12[k]);
 				if(err_t < err) {
-					R[0] = R_t[0];
-					R[1] = R_t[1];
-					R[2] = R_t[2];
+					idx[0] = i;
+					idx[1] = j;
+					idx[2] = k;
 					err = err_t;
 				}
+				if(err == 0.0) {
+					exit_search = 1;
+					break;
+				}
 			}
+			if(exit_search) {
+				break;
+			}
+		}
+		if(exit_search) {
+			break;
 		}
 	}
 		
 	int c = 0;
 	for(i = 0; i < 3; i++) {
-		c += R[i] != 0.0;
+		c += E12[idx[i]] != 0.0;
 	}
 	
-	res_array[0] = R[0];	
-	res_array[1] = R[1];	
-	res_array[2] = R[2];	
+	res_array[0] = E12[idx[0]];	
+	res_array[1] = E12[idx[1]];	
+	res_array[2] = E12[idx[2]];	
 
 	return c;
 }
